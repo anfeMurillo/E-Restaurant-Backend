c8e04c6ad1f482af9833f742b5309493a6cc55ab
anfeMurillo <anfehumu2.0@hotmail.com>
2025-12-10 19:26:45 -0500

    fix:Corregir error  de nombre en el contrato restaurantStaff.

--- src/contracts/restaurantStaff/restaurantStaff.go
+++ src/contracts/restaurantStaff/restaurantStaff.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Repository interface {
-	CreateStaffMember(ctx context.Context, staff *restaurantstaff.RestaurantStaff) (*restaurantstaff.RestaurantStaff, error)
+	Create(ctx context.Context, staff *restaurantstaff.RestaurantStaff) (*restaurantstaff.RestaurantStaff, error)
 
-	DeleteStaffMember(ctx context.Context, restaurantId int, userId int) error
+	Delete(ctx context.Context, restaurantId int, userId int) error
 
-	GetStaffByRestaurant(ctx context.Context, restaurantId int, onlyActive bool) ([]*userapp.User, error)
+	GetByRestaurant(ctx context.Context, restaurantId int, onlyActive bool) ([]*userapp.User, error)
 }
